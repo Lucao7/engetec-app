279ee3a1d864b5f5c9aca8fef14f32bef7229243
Lucas Ramos <codinglucao@gmail.com>
2023-08-25 17:47:29 -0300

    feat: criação tela de reset de senha

--- frontend/engetec-app/src/app/app.module.ts
+++ frontend/engetec-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ForgotpassComponent } from './pages/forgotpass/forgotpass.component';
+import { ResetpassComponent } from './pages/resetpass/resetpass.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { ForgotpassComponent } from './pages/forgotpass/forgotpass.component';
     LoginComponent,
     RegisterComponent,
     ForgotpassComponent,
+    ResetpassComponent,
   ],
   imports: [
     BrowserModule,
