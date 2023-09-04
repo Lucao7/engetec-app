2b895609ff7b22d6a7822ce26206ef369c4249d8
Lucas Ramos <codinglucao@gmail.com>
2023-09-04 16:35:05 -0300

    feat: forgotpass adicionado

--- frontend/engetec-app/src/app/forgotpass/forgotpass.component.ts
+++ frontend/engetec-app/src/app/forgotpass/forgotpass.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+// import { ForgotPassService } from '../../services/forgot/forgot-pass.service';
+import { first } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgotpass',
@@ -6,5 +10,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./forgotpass.component.scss']
 })
 export class ForgotpassComponent {
+
+  constructor(
+    // private forgotService: ForgotPassService,
+    private router: Router
+  ) { }
+
+  forgotForm : FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+  error = '';
+
+  get f() { return this.forgotForm.controls; };
+
+  forgotPass() {
+    if(this.forgotForm.invalid) {
+      console.log('Deu ruim!');
+      return;
+    }
+
+    console.log('Deu bom!');
+
+    // this.forgotService.forgot(this.f.email.value).pipe(first()).subscribe(
+    //   data => {
+    //     this.router.navigate(['/forgotpassmessage']);
+    //   },
+    //   error => {
+    //     this.error = error;
+    //     console.log("Forgot page error", this.error)
+    //   }
+    // )
+  }
 
 }
