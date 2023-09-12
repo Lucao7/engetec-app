d47fa9811e8f1cb5f0de8903c6dd4b8e19bed04b
Lucas Ramos <codinglucao@gmail.com>
2023-09-12 15:14:34 -0300

    fix: add provider for HttpClient

--- frontend/engetec-app/src/app/app.module.ts
+++ frontend/engetec-app/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,7 +18,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    HttpClientModule
   ],
   providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
