f476f6a96f2a2eef0bfb6698b87554505c8bad72
Lucas Ramos <codinglucao@gmail.com>
2023-09-18 17:28:18 -0300

    feat: enviar artigo

--- frontend/engetec-app/src/app/pages/artigo/artigo.component.ts
+++ frontend/engetec-app/src/app/pages/artigo/artigo.component.ts
@@ -1,10 +1,45 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AreaService } from 'src/app/_services/area/area.service';
+import { Area } from 'src/app/_models/area';
 
 @Component({
   selector: 'app-artigo',
   templateUrl: './artigo.component.html',
   styleUrls: ['./artigo.component.scss']
 })
-export class ArtigoComponent {
+export class ArtigoComponent implements OnInit {
+
+  areas: Area[] = [];
+
+  constructor(
+    private toast: ToastrService,
+    private router: Router,
+    private areaService: AreaService,
+  ) { }
+
+  artigoForm : FormGroup = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    group: new FormControl('', [Validators.required]),
+    areas: new FormControl('', [Validators.required]),
+  });
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  enviar() {
+
+  }
+
+  getCategories() {
+    this.areaService.getAreas()
+      .subscribe(
+        // areas => this.areas = areas,
+        error => console.log("Get Areas - ", error),
+      );
+  }
 
 }
--- frontend/engetec-app/src/app/pages/artigo/artigo.module.ts
+++ frontend/engetec-app/src/app/pages/artigo/artigo.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AngularMaterialModule } from '../../angular-material/angular-material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ArtigoRoutingModule } from './artigo-routing.module';
 import { ArtigoComponent } from './artigo.component';
@@ -11,7 +13,10 @@ import { ArtigoComponent } from './artigo.component';
   ],
   imports: [
     CommonModule,
-    ArtigoRoutingModule
+    ArtigoRoutingModule,
+    AngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ArtigoModule { }
