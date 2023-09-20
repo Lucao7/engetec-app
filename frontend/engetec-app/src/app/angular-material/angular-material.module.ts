d123ee388c293beec0369d4a2ca34a3938f535b2
Lucas Ramos <codinglucao@gmail.com>
2023-09-20 16:02:35 -0300

    build: add ngx material file input

--- frontend/engetec-app/src/app/angular-material/angular-material.module.ts
+++ frontend/engetec-app/src/app/angular-material/angular-material.module.ts
@@ -6,6 +6,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MaterialFileInputModule } from 'ngx-material-file-input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -21,12 +22,13 @@ import { MatSliderModule } from '@angular/material/slider';
     MatButtonModule,
     MatCardModule,
     MatInputModule,
+    MaterialFileInputModule,
     MatDividerModule,
     MatSelectModule,
     MatDatepickerModule,
     MatCheckboxModule,
     MatExpansionModule,
-    MatSliderModule
+    MatSliderModule,
   ]
 })
 export class AngularMaterialModule { }
