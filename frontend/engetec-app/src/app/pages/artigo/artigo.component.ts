f0d3e750f6077c6c296dc30aa629aa82cccbd013
Lucas Ramos <codinglucao@gmail.com>
2023-09-20 17:44:49 -0300

    feat(artigo): get areas & file input

--- frontend/engetec-app/src/app/pages/artigo/artigo.component.ts
+++ frontend/engetec-app/src/app/pages/artigo/artigo.component.ts
@@ -1,5 +1,5 @@
 import { Router } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AreaService } from 'src/app/_services/area/area.service';
@@ -22,9 +22,13 @@ export class ArtigoComponent implements OnInit {
 
   artigoForm : FormGroup = new FormGroup({
     title: new FormControl('', [Validators.required]),
+    keywords: new FormControl('', [Validators.required]),
     group: new FormControl('', [Validators.required]),
     areas: new FormControl('', [Validators.required]),
+    artigo: new FormControl(undefined, [Validators.required]),
   });
+
+  get f() { return this.artigoForm.controls; };
 
   ngOnInit(): void {
     this.getCategories();
@@ -35,11 +39,27 @@ export class ArtigoComponent implements OnInit {
   }
 
   getCategories() {
-    this.areaService.getAreas()
-      .subscribe(
-        // areas => this.areas = areas,
-        error => console.log("Get Areas - ", error),
-      );
+    // this.areaService.getAreas()
+    //   .subscribe(
+    //     areas => {
+    //       this.areas = areas
+    //     }
+    //     error => {
+    //       console.log("Get Areas error - ", error);
+    //       error.message.forEach((element: string | undefined) => {
+    //         this.toast.error(element, 'Login')
+    //       });
+    //     }
+    //   );
+
+    let area1 = {"id": 1, "name": "Análise e Desenvolvimento de Sistemas"};
+    let area2 = {"id": 2, "name": "Recursos Humanos"};
+    let area3 = {"id": 3, "name": "Comércio Exterior"};
+    let area4 = {"id": 4, "name": "Desenvolvimento de Software Multiplataforma"};
+    this.areas.push(area1);
+    this.areas.push(area2);
+    this.areas.push(area3);
+    this.areas.push(area4);
   }
 
 }
