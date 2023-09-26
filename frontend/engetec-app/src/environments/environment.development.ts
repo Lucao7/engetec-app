36a9ca505dc681a1c3288c05678b50f388082fd2
Lucas Ramos <codinglucao@gmail.com>
2023-09-26 17:57:49 -0300

    change & add urls to environment

--- frontend/engetec-app/src/environments/environment.development.ts
+++ frontend/engetec-app/src/environments/environment.development.ts
@@ -1,5 +1,8 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8070/',
-    authUrl: 'http://localhost:8070/auth/',
+    apiUrl: 'http://localhost:8080/api',
+    alunoUrl: 'http://localhost:8080/api/aluno',
+    avaliadorUrl: 'http://localhost:8080/api/avaliador',
+    authUrl: 'http://localhost:8080/api/auth/',
+    getDocumentUrl: 'http://localhost:8080/user/document',
 };
--- frontend/engetec-app/src/environments/environment.ts
+++ frontend/engetec-app/src/environments/environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:8070/',
-    authUrl: 'http://localhost:8070/auth/',
+    apiUrl: 'http://localhost:8080/api',
+    alunoUrl: 'http://localhost:8080/api/aluno',
+    avaliadorUrl: 'http://localhost:8080/api/avaliador',
+    authUrl: 'http://localhost:8080/api/auth/',
+    getDocumentUrl: 'http://localhost:8080/user/document',
 };
