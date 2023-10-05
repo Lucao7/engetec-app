d5f5a9e63a763661685a169bbdb557b4b11a1741
Lucas Ramos <codinglucao@gmail.com>
2023-10-04 22:26:18 -0300

    feat: add user url

--- frontend/engetec-app/src/environments/environment.ts
+++ frontend/engetec-app/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     apiUrl: 'http://localhost:8080/api',
+    userUrl: 'http://localhost:8080/api/usuario',
     alunoUrl: 'http://localhost:8080/api/aluno',
     avaliadorUrl: 'http://localhost:8080/api/avaliador',
     authUrl: 'http://localhost:8080/api/login',
