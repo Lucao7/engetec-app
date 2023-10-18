3d86ba320d15500799e7acffdae073948375c21d
Lucas Ramos <codinglucao@gmail.com>
2023-10-18 17:36:00 -0300

    fix: update backend routing

--- frontend/engetec-app/src/environments/environment.development.ts
+++ frontend/engetec-app/src/environments/environment.development.ts
@@ -1,9 +1,9 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8080/api',
-    userUrl: 'http://localhost:8080/api/usuario',
-    alunoUrl: 'http://localhost:8080/api/aluno',
-    avaliadorUrl: 'http://localhost:8080/api/avaliador',
-    authUrl: 'http://localhost:8080/api/login',
-    getDocumentUrl: 'http://localhost:8080/user/document',
+  production: false,
+  apiUrl: 'http://localhost:8080',
+  userUrl: 'http://localhost:8080/usuario',
+  alunoUrl: 'http://localhost:8080/aluno',
+  avaliadorUrl: 'http://localhost:8080/avaliador',
+  authUrl: 'http://localhost:8080/auth/login',
+  getDocumentUrl: 'http://localhost:8080/user/document',
 };
--- frontend/engetec-app/src/environments/environment.ts
+++ frontend/engetec-app/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:8080/api',
-    userUrl: 'http://localhost:8080/api/usuario',
-    alunoUrl: 'http://localhost:8080/api/aluno',
-    avaliadorUrl: 'http://localhost:8080/api/avaliador',
-    authUrl: 'http://localhost:8080/api/login',
+    apiUrl: 'http://localhost:8080',
+    userUrl: 'http://localhost:8080/usuario',
+    alunoUrl: 'http://localhost:8080/aluno',
+    avaliadorUrl: 'http://localhost:8080/avaliador',
+    authUrl: 'http://localhost:8080/auth/login',
     getDocumentUrl: 'http://localhost:8080/user/document',
 };
