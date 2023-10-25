40d83ed35515c68b66fc4dc2576f168553a25711
Lucas Ramos <codinglucao@gmail.com>
2023-10-25 16:18:51 -0300

    fix: proxy not working

--- frontend/engetec-app/src/environments/environment.development.ts
+++ frontend/engetec-app/src/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  userUrl: 'usuario',
-  alunoUrl: 'aluno',
-  avaliadorUrl: 'avaliador',
-  authUrl: 'auth/login',
-  getDocumentUrl: 'user/document',
-  areasUrl: 'areas',
-  avaliacaoUrl: 'avaliacao',
+  userUrl: 'http://localhost:8080/usuario',
+  alunoUrl: 'http://localhost:8080/aluno',
+  avaliadorUrl: 'http://localhost:8080/avaliador',
+  authUrl: 'http://localhost:8080/auth/login',
+  getDocumentUrl: 'http://localhost:8080/user/document',
+  areasUrl: 'http://localhost:8080/areas',
+  avaliacaoUrl: 'http://localhost:8080/avaliacao',
 };
